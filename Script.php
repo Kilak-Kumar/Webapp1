f306a97d356bca4f8ac421cf283499a3e6423a5d
Kilak-Kumar <44109305+Kilak-Kumar@users.noreply.github.com>
2018-10-23 00:21:50 +0530

    Create Script.php

---
+++ Script.php
@@ -0,0 +1,41 @@
+<?php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:univers.database.windows.net,1433; Database = UniversityDB", "Hanu", "Kilak@7248321326");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	print("Connection is there \n");
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+
+Print("Taking Values \n");
+
+$id=$_POST["id"];
+$roll=$_POST["roll"];
+$fname=$_POST["fname"];
+$lname=$_POST["lname"];
+$gpa=$_POST["gpa"];
+$major=$_POST["major"];
+
+
+$data = [
+    'id1' => $id,
+	'roll1' => $roll,
+    'fname1' => $fname,
+    'lname1' => $lname,
+	'gpa1' => $gpa,
+	'major1' => $major,
+	
+];
+$sql = "INSERT INTO TopStudents (Id,RolLNo,FirstName,LastName,AvgGPA,Major) VALUES (:id1, :roll1, :fname1, :lname1,:gpa1,:major1)";
+
+
+$stmt= $conn->prepare($sql);
+$stmt->execute($data);
+Print("Ending program");
+    header("Location:login.html");
+
+$conn.close();
+?>
