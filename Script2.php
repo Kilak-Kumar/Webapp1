02f35b66086ad69018c8d9ae4b53ee9b44d422fd
Kilak-Kumar <44109305+Kilak-Kumar@users.noreply.github.com>
2018-10-23 00:23:14 +0530

    Create Script2.php

---
+++ Script2.php
@@ -0,0 +1,46 @@
+<?php
+	
+try
+ {
+    $conn = new PDO("sqlsrv:server = tcp:univers.database.windows.net,1433; Database = UniversityDB", "Kilak", "Hanu@7248321326");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+$roll=$_POST["roll"];
+$sql="Select * from TopStudents where RollNo=$roll";
+
+
+
+try
+ {
+   $rows=$conn->query($sql);
+ } 
+catch(PDOException $r)
+ { 
+	print("$r");
+ }
+
+
+
+foreach ($rows as  $row)
+{
+   if ($row!=NUll)
+   {
+	$FName = $row['FirstName'];
+	$LName = $row['LastName']
+	$GPA  = $row['GPA'];
+	$M= $row['Major'];
+   }
+   else
+	{
+	print("NO RECORD FOUND !!");	
+	}
+}
+print("FirstName :- '$FName' <br>LastName:- '$LName'<br> GPA :- '$GPA' <BR> Major Project :- '$M'");
+
+$conn.close();
+?>
